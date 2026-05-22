e3ff7a1fd884fab129b50f91d77018f0d5320f8e
Drishya U Hegde <drishyahegde123@gmail.com>
2026-05-22 12:47:21 +0530

    Enhance house prediction with user input
    Added user input for house features and prediction output.

--- house_prediction.py
+++ house_prediction.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_absolute_error
 
 data = pd.read_csv("HousingData.csv")
 
@@ -17,9 +16,26 @@ X_train, X_test, y_train, y_test = train_test_split(
 model = LinearRegression()
 model.fit(X_train, y_train)
 
-predictions = model.predict(X_test)
+print("===== Boston House Price Prediction =====")
 
-error = mean_absolute_error(y_test, predictions)
+CRIM = float(input("Enter crime rate: "))
+ZN = float(input("Enter residential land zone value: "))
+INDUS = float(input("Enter industrial area proportion: "))
+CHAS = int(input("Near Charles River? (0 or 1): "))
+NOX = float(input("Enter nitric oxide concentration: "))
+RM = float(input("Enter average number of rooms: "))
+AGE = float(input("Enter house age: "))
+DIS = float(input("Enter distance to employment centers: "))
+RAD = int(input("Enter highway accessibility index: "))
+TAX = float(input("Enter property tax rate: "))
+PTRATIO = float(input("Enter pupil-teacher ratio: "))
+B = float(input("Enter proportion of black population value: "))
+LSTAT = float(input("Enter lower status population percentage: "))
 
-print("Model trained successfully!")
-print("Mean Absolute Error:", error)
+user_data = pd.DataFrame([[CRIM, ZN, INDUS, CHAS, NOX, RM,
+                           AGE, DIS, RAD, TAX, PTRATIO, B, LSTAT]],
+columns=X.columns)
+
+prediction = model.predict(user_data)
+
+print("\nPredicted House Price:", prediction[0])
